f70efa1e61dd343a65f53b151ec1ca8359370e20
MAjavanta <mahma@LAPTOP-5DR8I9FI.>
2025-06-06 00:49:08 +0100

    Add editing keymaps

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ opt.termguicolors = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
+opt.expandtab = true
 opt.smartindent = true
 opt.wrap = false
 opt.confirm = true
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,23 +2,33 @@ local keymap = vim.keymap
 
 local opts = { noremap = true, silent = true }
 -- Directory Navigation
-keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', opts)
+keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", opts)
 
 -- Navigating panes
-keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Window management
-keymap.set('n', '<leader>sv', ':vsplit<CR>', opts)
-keymap.set('n', '<leader>sh', ':split<CR>', opts)
-keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>', opts)
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
+keymap.set("n", "<leader>sh", ":split<CR>", opts)
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts)
 
-keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Indenting
-keymap.set('v', '<', '<gv')
-keymap.set('v', '>', '>gv')
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+--Control J/K to move lines down/up in visual mode
+keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv", { desc = "moves line down in visual selection" })
+keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv", { desc = "moves line up in visual selection" })
+
+--paste over text without writing deleted text to clipboard
+keymap.set({ "n", "v" }, "<leader>d", [["_dP]])
+--deletes character without copying to clipboard
+keymap.set("n", "x", "_x")
+
+--Replace word under cursor globally
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
